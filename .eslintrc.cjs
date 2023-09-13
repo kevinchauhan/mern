42a975dc0cd49214bfae3afea7c570d65c6df618
kevin <rwnkevinchauhan@gmail.com>
2023-09-13 16:50:03 +0530

    setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsConfigRootDir: '_dirname',
+        project: ['./tsconfig.json'],
+    },
+    plugins: ['@typescript-eslint'],
+    root: true,
+    rules: {
+        'no-console': 'error',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-empty-function': 'warn',
+    },
+};
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+const foo = () => {return 0};
