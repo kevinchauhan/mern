087ca92eccca38619bfdf9727a2968b015f4a72c
kevin <rwnkevinchauhan@gmail.com>
2023-09-13 20:40:09 +0530

    setup express server

---
+++ server.ts
@@ -0,0 +1,16 @@
+import { CONFIG } from './src/config';
+import app from './src/app';
+
+const PORT = CONFIG.PORT || 8000;
+
+const startServer = async (port: number) => {
+    try {
+        app.listen(port, () => {
+            // console.log(`Listening on port ${port}`)
+        });
+    } catch (error: unknown) {
+        // console.log(`something went wrong..`, error)
+    }
+};
+
+startServer(PORT as number);
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+// Routes
+app.get('/', (req, res) => {
+    res.send('ok...');
+});
+export default app;
