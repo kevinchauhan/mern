d6fa94f96b070e8bda76b0538be2ed0133b97664
kevin <rwnkevinchauhan@gmail.com>
2023-09-15 20:27:29 +0530

    setup logger

--- server.ts
+++ server.ts
@@ -1,15 +1,21 @@
 import { CONFIG } from './src/config'
 import app from './src/app'
+import logger from './src/config/logger'
 
 const PORT = CONFIG.PORT || 8000
 
 const startServer = async (port: number) => {
     try {
         app.listen(port, () => {
-            // console.log(`Listening on port ${port}`)
+            logger.info(`Listening on port ${port}`)
         })
     } catch (error: unknown) {
-        // console.log(`something went wrong..`, error)
+        if (error instanceof Error) {
+            logger.error(`something went wrong..`, error)
+            setTimeout(() => {
+                process.exit(1)
+            }, 1000)
+        }
     }
 }
 
