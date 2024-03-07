befbfd9fc6f5056213b991fee5530c5e4b0a18d4
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-03-07 15:08:39 +0530

    add error handling and logger

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,16 +1,20 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { UserService } from "../services/UserService";
+import { Logger } from "winston";
 
 export class AuthController {
-    userService: UserService
 
-    constructor(userService: UserService) {
-        this.userService = userService
-    }
+    constructor(private userService: UserService, private logger: Logger) { }
 
-    async register(req: Request, res: Response) {
+    async register(req: Request, res: Response, next: NextFunction) {
         const { firstName, lastName, email, password } = req.body
-        const id = await this.userService.create({ firstName, lastName, email, password })
-        res.status(201).json({ id })
+        this.logger.debug('New request to register a user', { firstName, lastName, email, password: '****' })
+        try {
+            const user = await this.userService.create({ firstName, lastName, email, password })
+            this.logger.info('User has been registered', { id: user.id })
+            res.status(201).json({ id: user.id })
+        } catch (error) {
+            next(error)
+        }
     }
 }
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,13 +1,19 @@
 import { UserData } from "../types"
 import { User } from "../entity/User"
 import { Repository } from "typeorm"
+import createHttpError from "http-errors"
 
 export class UserService {
 
     constructor(private userRepository: Repository<User>) { }
 
     async create({ firstName, lastName, email, password }: UserData) {
-        const user = await this.userRepository.save({ firstName, lastName, email, password })
-        return user.id
+        try {
+            const user = await this.userRepository.save({ firstName, lastName, email, password })
+            return user
+        } catch (error) {
+            const err = createHttpError(500, 'Failed to store data in db')
+            throw err
+        }
     }
 }
