2cd8b1bcae0334117149ba85e8078cb763b9ff42
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-03-09 14:23:28 +0530

    refactor tokens

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,8 +3,6 @@ import { UserService } from "../services/UserService";
 import { Logger } from "winston";
 import { validationResult } from "express-validator";
 import { JwtPayload } from "jsonwebtoken";
-import { AppDataSource } from '../config/data-source';
-import { RefreshToken } from '../entity/RefreshToken';
 import { UserRequest } from '../types';
 import { TokenService } from '../services/TokenService';
 
@@ -14,13 +12,19 @@ export class AuthController {
 
     async register(req: UserRequest, res: Response, next: NextFunction) {
         const { firstName, lastName, email, password } = req.body
+
+        // validation
         const result = validationResult(req)
         if (!result.isEmpty()) {
             res.status(400).json({ errors: result.array() })
         }
+
         this.logger.debug('New request to register a user', { firstName, lastName, email, password: '****' })
+
         try {
+            // persists user
             const user = await this.userService.create({ firstName, lastName, email, password })
+
             this.logger.info('User has been registered', { id: user.id })
 
             const payload: JwtPayload = {
@@ -30,13 +34,8 @@ export class AuthController {
 
             const accessToken = this.tokenService.generateAccessToken(payload)
 
-            // persist refreshtoken
-            const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365
-            const refreshTokenRepository = AppDataSource.getRepository(RefreshToken)
-            const newRefreshToken = await refreshTokenRepository.save({
-                user: user,
-                expiresAt: new Date(Date.now() + MS_IN_YEAR)
-            })
+            // persist refresh token
+            const newRefreshToken = await this.tokenService.persistsRefreshToken(user)
 
             const refreshToken = this.tokenService.generateRefreshToken({ ...payload, id: newRefreshToken.id })
 
--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -3,8 +3,13 @@ import createHttpError from "http-errors";
 import { JwtPayload, sign } from "jsonwebtoken";
 import path from "path";
 import { CONFIG } from "../config";
+import { User } from "../entity/User";
+import { RefreshToken } from "../entity/RefreshToken";
+import { Repository } from "typeorm";
 
 export class TokenService {
+
+    constructor(private refreshTokenRepository: Repository<RefreshToken>) { }
 
     generateAccessToken(payload: JwtPayload) {
         let privateKey: Buffer
@@ -31,6 +36,15 @@ export class TokenService {
             jwtid: String(payload.id)
         })
         return refreshToken
+    }
+
+    async persistsRefreshToken(user: User) {
+        const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365
+        const newRefreshToken = await this.refreshTokenRepository.save({
+            user: user,
+            expiresAt: new Date(Date.now() + MS_IN_YEAR)
+        })
+        return newRefreshToken
     }
 
 }
