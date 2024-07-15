740888fc167cbf7405728393331e2e84d0f8b4cc
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-07-15 21:31:45 +0530

    add tenenst table

---
+++ src/entity/Tenant.ts
@@ -0,0 +1,20 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+
+@Entity({ name: 'tenants' })
+export class Tenants {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({ type: 'varchar', length: 100 })
+    name: string
+
+    @Column({ type: 'varchar', length: 255 })
+    address: string
+
+    @UpdateDateColumn()
+    updatedAt: number
+
+    @CreateDateColumn()
+    createdAt: number
+}
